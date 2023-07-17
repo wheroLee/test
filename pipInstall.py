0d1f42be3e0061c62692d081a761bd5d7d958eff
wheroLee <wirolee@gmail.com>
2023-07-17 14:03:19 +0900

    commit 20230717

--- pipInstall.py
+++ pipInstall.py
@@ -8,27 +8,48 @@ def checkAndInstall(package):
         import package
     except:
         # pip 모듈 업그레이드
-        subprocess.check_call([sys.executable,'-m', 'pip', 'install', '--upgrade', 'pip'])
+        subprocess.check_call(
+            [sys.executable, "-m", "pip", "install", "--upgrade", "pip"]
+        )
         # 에러 발생한 모듈 설치
-        subprocess.check_call([sys.executable,'-m', 'pip', 'install', '--upgrade', package])
+        subprocess.check_call(
+            [sys.executable, "-m", "pip", "install", "--upgrade", package]
+        )
         # 다시 import
         import package
 
+
 def install(package, upgrade=True):
     import pip
-    if hasattr(pip, 'main'):
+
+    if hasattr(pip, "main"):
         if upgrade:
-            pip.main(['install','--upgrade',package])
+            pip.main(["install", "--upgrade", package])
         else:
-            pip.main(['install',package])
+            pip.main(["install", package])
     else:
         if upgrade:
-            pip._internal.main(['install','--upgrade',package])
+            pip._internal.main(["install", "--upgrade", package])
         else:
-            pip._internal.main(['install',package])
+            pip._internal.main(["install", package])
     try:
         eval(f"import {package}")
     except ModuleNotFoundError:
         print("# Package name might be different. please check it")
     except Exception as e:
         print(e)
+
+
+if __name__ == "__main__":
+    # %% lib check and install
+
+    liblst = [
+        "opencv-python",
+        "opencv-contrib-python",
+        "opencv-contrib-python==4.6.0.66",
+        "numpy",
+        "matplotlib",
+        "pandas",
+    ]
+    for lib in liblst:
+        ret = install(lib)
